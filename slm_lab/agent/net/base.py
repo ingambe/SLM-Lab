233ebae53a2de19d936dcecde4ac9b9d0caa6061
Pierre Tassel <pierre.tassel@outlook.fr>
2020-07-22 12:13:11 +0200

    prepare for benchmark

--- slm_lab/agent/net/base.py
+++ slm_lab/agent/net/base.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from slm_lab.agent.net import net_util
+from collections import deque
 import pydash as ps
 import torch
 import torch.nn as nn
@@ -18,6 +19,12 @@ class Net(ABC):
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.grad_norms = None  # for debugging
+        self.previous_grads = None
+        if not ps.is_empty(self.net_spec.get('avg_grad_clipper')):
+            avg_grad_clipper = self.net_spec.get('avg_grad_clipper')
+            self.grad_scaler = float(avg_grad_clipper['avg_scaler'])
+            stack_size = int(avg_grad_clipper['n_last'])
+            self.previous_grads = deque(maxlen=stack_size)
         if self.net_spec.get('gpu'):
             if torch.cuda.device_count():
                 self.device = f'cuda:{net_spec.get("cuda_id", 0)}'
@@ -37,7 +44,19 @@ class Net(ABC):
             lr_scheduler.step(epoch=ps.get(clock, 'frame'))
         optim.zero_grad()
         loss.backward()
-        if self.clip_grad_val is not None:
+        if self.previous_grads is not None:
+            total_norm = 0.0
+            for p in self.parameters():
+                param_norm = p.grad.data.norm(2)
+                total_norm += param_norm.item() ** 2
+            total_norm = total_norm ** (1. / 2)
+            self.previous_grads.append(total_norm)
+            average_grad = sum(self.previous_grads) / len(self.previous_grads)
+            if self.clip_grad_val is not None:
+                nn.utils.clip_grad_norm_(self.parameters(), min(average_grad * self.grad_scaler, self.clip_grad_val))
+            else:
+                nn.utils.clip_grad_norm_(self.parameters(), average_grad * self.grad_scaler)
+        elif self.clip_grad_val is not None:
             nn.utils.clip_grad_norm_(self.parameters(), self.clip_grad_val)
         if global_net is not None:
             net_util.push_global_grads(self, global_net)
